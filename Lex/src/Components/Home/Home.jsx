d1465bebfd1eda7987a9ef480d21468dd33d5806
Ayush Pandey <apayush.20@gmail.com>
2025-08-08 11:52:03 +0530

    Updated

--- Lex/src/Components/Home/Home.jsx
+++ Lex/src/Components/Home/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
                     <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-left sm:text-left text-white" >
                     <h2 className='text-[#a90c0c] text-3xl font-bold animate-fadeInUp'>Journal of Social
                         <br></br> & Legal Studies</h2>
-                        <p className='text-lg animate-fadeInUp'>Lex Revolution is an international, peer-reviewed, quarterly research journal focusing on social and legal studies. It invites original, unpublished research papers, articles, short comments, and book reviews from students, scholars, teachers, and professionals in the fields of law, human rights, and social sciences.</p>
+                        <p className='text-lg animate-fadeInUp'>Lex Revolution is an international, peer-reviewed, Quarterly published research journal focusing on social and legal studies. It invites original, unpublished research papers, articles, short comments, and book reviews from students, scholars, teachers, and professionals in the fields of law, human rights, and social sciences.</p>
                     </div>
                 </div>
 
